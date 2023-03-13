5ebb5ab9a84d9c22c4741c57947d5c721592fc03
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-03-13 13:30:16 +0300

    Тесты для forms

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.core.paginator import Page
 from django.test import Client, TestCase
 from django.urls import reverse
 
